079f05240b02cbd556b1e75be8d3d8e6b270a257
Chris Hyman <chris@wugs.me>
2025-03-13 01:50:38 -0400

    add CorsConfig and properties

---
+++ src/main/java/me/wugs/judy/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package me.wugs.judy.config;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class CorsConfig {
+
+  private final CorsProperties corsProperties;
+
+  @Bean
+  public CorsFilter corsFilter() {
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(corsProperties.getAllowedOrigins());
+    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+    config.setAllowCredentials(true);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", config);
+
+    return new CorsFilter(source);
+  }
+}
---
+++ src/main/java/me/wugs/judy/config/CorsProperties.java
@@ -0,0 +1,15 @@
+package me.wugs.judy.config;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+  private List<String> allowedOrigins;
+}
